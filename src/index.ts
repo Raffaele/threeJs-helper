3cc0b9f8174783ce659a0e76cfac6e3eac693e78
Raffaele <mori.raffaele@gmail.com>
2026-07-09 22:24:04 +0100

    Extra helpers added

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,29 @@ camera.position.set(-10, 30, 30);
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.enableDamping = true;
 
-const axesHelper = new THREE.AxesHelper(5);
+const axesHelper = new THREE.AxesHelper(15);
 scene.add(axesHelper);
 
-const gridHelper = new THREE.GridHelper(30);
-scene.add(gridHelper);
+const gridHelperX = new THREE.GridHelper(30, 10, 0xff0000, 0xff0000);
+gridHelperX.rotation.z = Math.PI / 2;
+gridHelperX.visible = false;
+scene.add(gridHelperX);
+
+const gridHelperY = new THREE.GridHelper(30);
+gridHelperY.visible = false;
+scene.add(gridHelperY);
+
+const gridHelperZ = new THREE.GridHelper(30, 10, 0x0000ff, 0x0000ff);
+gridHelperZ.rotation.x = Math.PI / 2;
+gridHelperZ.visible = false;
+scene.add(gridHelperZ);
+
+const cubeGeometry = new THREE.BoxGeometry();
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+cube.add(new THREE.AxesHelper(3));
+cube.position.set(0, 5, 0);
 
 
 function animate() {
@@ -27,4 +45,51 @@ function animate() {
 }
 animate();
 
+const gui = new dat.GUI();
+const params = {
+  isAxesHelperVisible: true,
+  isGridHelperXVisible: false,
+  isGridHelperYVisible: false,
+  isGridHelperZVisible: false,
+  isCubeAxesHelperVisible: true,
+  isCubeVisible: true,
+  cubeX: cube.position.x,
+  cubeY: cube.position.y,
+  cubeZ: cube.position.z,
+};
 
+gui.add(params, 'isAxesHelperVisible').name('Axes Helper').onChange((value: boolean) => {
+  axesHelper.visible = value;
+});
+
+gui.add(params, 'isGridHelperXVisible').name('Grid Helper X').onChange((value: boolean) => {
+  gridHelperX.visible = value;
+});
+
+gui.add(params, 'isGridHelperYVisible').name('Grid Helper Y').onChange((value: boolean) => {
+  gridHelperY.visible = value;
+});
+
+gui.add(params, 'isGridHelperZVisible').name('Grid Helper Z').onChange((value: boolean) => {
+  gridHelperZ.visible = value;
+});
+
+gui.add(params, 'isCubeAxesHelperVisible').name('Cube Axes Helper').onChange((value: boolean) => {
+  cube.children[0].visible = value;
+});
+
+gui.add(params, 'isCubeVisible').name('Cube').onChange((value: boolean) => {
+  cube.visible = value;
+});
+
+gui.add(params, 'cubeX', -10, 10).name('Cube X').onChange((value: number) => {
+  cube.position.x = value;
+});
+
+gui.add(params, 'cubeY', -10, 10).name('Cube Y').onChange((value: number) => {
+  cube.position.y = value;
+});
+
+gui.add(params, 'cubeZ', -10, 10).name('Cube Z').onChange((value: number) => {
+  cube.position.z = value;
+});
